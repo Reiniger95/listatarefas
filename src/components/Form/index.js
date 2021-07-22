a2f594bfca754034d9b9b00a2f2b5e6d0cbd0a79
Reiniger95 <reiniger.95@gmail.com>
2021-07-22 11:51:15 -0300

    Initial Commit

---
+++ src/components/Form/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FaPlus } from 'react-icons/fa';
+import './Form.css';
+import PropTypes from 'prop-types';
+
+export default function Form({ handleChange, handleSubmit, novaTarefa }) {
+  return (
+    <form onSubmit={handleSubmit} action="#" className="form">
+      <input onChange={handleChange} type="text" value={novaTarefa} />
+      <button type="submit">
+        <FaPlus />
+
+      </button>
+
+    </form>
+  );
+}
+
+Form.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  novaTarefa: PropTypes.string.isRequired,
+};
